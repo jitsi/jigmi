c8b363111931d118a47ef98dafdd995c9e9b5980
Brian Baldino <bbaldino@gmail.com>
2019-07-10 13:16:25 -0700

    get rid of multiple envs support (we only use one)

--- migrate.js
+++ migrate.js
@@ -8,11 +8,9 @@ const path = require('path');
 const childProcess = require('child_process');
 const process = require('process');
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '/config/config.json'))[env];
+const config = require(path.join(__dirname, '/config/config.json')).development;
 const Umzug = require('umzug');
 
-console.log(`Using database for environment ${env}`);
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 const umzug = new Umzug({
